a2e78249b15bb9ae052afe349219ba0093fed151
Crysicia <7459685+Crysicia@users.noreply.github.com>
2021-02-24 14:27:54 +0100

    Add ft_strdup

--- ft_strdup.s
+++ ft_strdup.s
@@ -2,29 +2,40 @@ section .text
 	global ft_strdup
 	extern malloc
 
+; input		- rsi
+; strlen	- rdxinfo regist
+; output	- rax
+; counter	- rcx
+
 ft_strdup:
-	mov rax, -1
+	mov		rcx, -1
+	mov		rsi, rdi
 
 strlen:
-	inc rax
-	cmp byte [rdi + rax], 0
+	inc rcx
+	cmp byte [rsi + rcx], 0
 	jnz strlen
+	mov rdx, rcx
+	mov rcx, -1
 
 alloc:
-	push rdi
-	mov rdi, rax
+	mov rdi, rdx
+	inc rdi
+	push rsi
 	call malloc wrt ..plt
-	pop rdi
 	cmp rax, 0
 	jz exit
+	xor rdx, rdx
+	mov rcx, -1
+	pop rsi
 
-; copy:
-; 	mov		al, [rsi]
-; 	mov		[rdi], al
-; 	inc		rdi
-; 	inc		rsi
-; 	cmp		al, 0
-; 	jnz		copy
+copy:
+	inc		rcx
+	mov		dl, [rsi + rcx]
+	mov	 	[rax + rcx], dl
+	cmp		dl, 0
+	jnz		copy
+	mov 	[rax + rcx], byte 0
 
 exit:
 	ret
--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+
 size_t	ft_strlen(char *str);
 ssize_t	ft_write(int fd, const void *str, unsigned int size);
 int ft_strcmp(const char *s1, const char *s2);
+char *ft_strdup(const char *s1);
 
 #define FT_STRLEN(str)			printf("{%s} - (System): %ld, (ft): %ld\n", str, strlen(str), ft_strlen(str));
 #define FT_WRITE(fd, str, size) printf("{%i, %s, %ld} - (System): %ld, (ft): %ld\n", fd, str, size, write(fd, str, size), ft_write(fd, str, size));
@@ -13,6 +15,7 @@ int main(int argc, char *argv[])
 	(void)argc;
 	FT_STRLEN(argv[1]);
 	FT_WRITE(2, argv[1], strlen(argv[1]));
-	ft_strcmp("Hello", "Hello");
+	// ft_strcmp("Hello", "Hello");
+	printf("%s\n", ft_strdup("Yo les gens"));
 	return 0;
 }
