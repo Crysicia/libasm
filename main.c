358ecac77657e51f136f39c4de1ec1d28f2ced91
Crysicia <7459685+Crysicia@users.noreply.github.com>
2021-03-09 16:38:47 +0100

    Add all test for mandatory part

--- main.c
+++ main.c
@@ -5,9 +5,6 @@
 #include <fcntl.h>
 #include <errno.h>
 
-#define SYSTEM "\e[0;95mSystem\e[0m"
-#define FT "\e[0;92mft\e[0m"
-
 #define READ_SIZE 10
 #define GREEN "\e[0;32m"
 #define RED "\e[0;31m"
@@ -19,18 +16,6 @@ ssize_t	ft_read(int fd, void *buf, size_t count);
 int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strdup(const char *s1);
 char	*ft_strcpy(char *dest, const char *src);
-
-int lulutest(int i);
-
-#define FT_STRCMP(s1, s2)		printf("{[%s], [%s]} - ("SYSTEM"): %i, ("FT"): %i\n", s1, s2, strcmp(s1, s2), ft_strcmp(s1, s2));
-#define FT_STRLEN(str)			printf("{%s} - (System): %ld, (ft): %ld\n", str, strlen(str), ft_strlen(str));
-#define FT_WRITE(fd, str, size) printf("{%i, %s, %ld} - (System): %ld, (ft): %ld\n", fd, str, size, write(fd, str, size), ft_write(fd, str, size));
-#define FT_STRDUP(str)			char *sys = strdup(str); \
-								char *ft = ft_strdup(str); \
-								printf("{%s} - (System): [%s], (ft): [%s]\n", str, sys, ft);\
-								free(sys); \
-								free(ft);
-
 
 void test_strlen(char *str)
 {
@@ -77,35 +62,53 @@ void test_read(char *filepath)
 	printf("%s(System): %4ld, (ft): %4ld - [%s]\n-- ERRNO: (System): %d, (ft): %d\n-- (System buffer): %s\n-- (Ft buffer): %s\n"RESET, status, system, ft, filepath, system_errno, ft_errno, system_buf, ft_buf);
 }
 
-// void test_write(int fd)
-// {
-
-// }
-
-// void test_strdup(char *str)
-// {
-
-// }
-
-// void test_strcpy(char *str)
-// {
-
-// }
-
-int main(int argc, char *argv[])
+void test_write(int fd, char *str, int size)
 {
-	(void)argc;
-	// printf("Greater: %i, Equal: %i, Less: %i\n", lulutest(50), lulutest(42), lulutest(0));
-	FT_STRLEN(argv[1]);
-	FT_WRITE(4, argv[1], strlen(argv[1]));
-	printf("--- \e[1;36mft_strlen\e[0m ---\n");
-	char dest[100];
+	char *status = RED;
 
+	size_t system = write(fd, str, size);
+	int system_errno = errno;
+	size_t ft = ft_write(fd, str, size);
+	int ft_errno = errno;
+
+	if (ft == system && ft_errno == system_errno)
+		status = GREEN;
+	printf("%s(System): %4ld, (ft): %4ld\n-- ERRNO: (System): %d, (ft): %d"RESET"\n", status, system, ft, system_errno, ft_errno);
+
+}
+
+void test_strdup(char *str)
+{
+	char *status = RED;
+	char *system = strdup(str);
+	char *ft = ft_strdup(str);
+
+	if (!strcmp(ft, system))
+		status = GREEN;
+	printf("%s(System): %s, (ft): %s - [%s]"RESET"\n", status, system, ft, str);
+	free(system);	
+	free(ft);	
+}
+
+void test_strcpy(char *str)
+{
+	char *status = RED;
+	char system_buf[100];
+	char ft_buf[100];
+	char *system = strcpy(system_buf, str);
+	char *ft = ft_strcpy(ft_buf, str);
+
+	if (!strcmp(ft, system) && !strcmp(ft_buf, system_buf))
+		status = GREEN;
+	printf("%s(System): %s, (ft): %s - [%s]"RESET"\n", status, system_buf, ft_buf, str);
+}
+
+int main(void)
+{
 	printf("--- \e[1;36mft_strlen\e[0m ---\n");
 	test_strlen("");
 	test_strlen("Bonjour");
-	test_strlen("Hello world!");
-
+	test_strlen("Hello, world!");
 
 	printf("--- \e[1;36mft_strcmp\e[0m ---\n");
 	test_strcmp("", "");
@@ -118,15 +121,24 @@ int main(int argc, char *argv[])
 	test_read("main.c");
 	test_read("Makefile");
 	test_read("ft_strlen.s");
-	printf("[%s]\n", ft_strcpy(dest, "hello"));
-	FT_STRCMP("bonjour", "bonjour");
-	FT_STRCMP("bonjour", "");
-	FT_STRCMP("", "bonjour");
-	FT_STRCMP("", "");
-	FT_STRCMP(" ", " ");
-	FT_STRCMP("Lorem", "Loreee");
-	FT_STRCMP("    ", " ");
-	FT_STRCMP("No", "ON");
-	FT_STRDUP("Hello everyone");
+	test_read("/dev/null");
+
+	printf("--- \e[1;36mft_write\e[0m ---\n");
+	test_write(0, "Hello, world!\n", 14);
+	test_write(0, "Hello, world!\n", 1);
+	test_write(1, "Hello, world!\n", 14);
+	test_write(2, "Hello, world!\n", 14);
+	test_write(42, "Hello, world!\n", 14);
+
+	printf("--- \e[1;36mft_strdup\e[0m ---\n");
+	test_strdup("");
+	test_strdup("Hi\0");
+	test_strdup("Hidden\0Secret");
+	test_strdup("Im a normal string");
+
+	printf("--- \e[1;36mft_strcpy\e[0m ---\n");
+	test_strcpy("test");
+	test_strcpy("\0");
+	test_strcpy("Copy copy copy");
 	return 0;
 }
