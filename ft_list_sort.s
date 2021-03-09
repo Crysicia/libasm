ff53eb10082bbf5a96849aa4bf356488734fe84f
Crysicia <7459685+Crysicia@users.noreply.github.com>
2021-03-09 12:24:36 +0100

    WORKING SORT

--- ft_list_sort.s
+++ ft_list_sort.s
@@ -14,18 +14,15 @@ check_error:
 	mov		rbx, [rdi]
 	cmp		rbx, 0
 	je		exit
-	jmp		sort
 
 sort:
 	cmp		r8, 0
 	je		exit
 	xor		r8, r8
-	mov		r10, [rdi]
 	mov		rbx, [rdi]
+	cmp		rbx, 0
+	je		exit
 	mov		rcx, [rbx + 8]
-	; xor		r10, r10
-	; mov		rbx, [rdi]
-	; mov		rcx, [rbx + 8]
 
 sort_loop:
 	cmp		rcx, 0
@@ -44,74 +41,20 @@ sort_loop:
 	pop		rcx
 	pop		rsi
 	pop		rdi
-	cmp		rax, 0
-	jge		swap
+	jg  	swap
 
 increment_loop:
-	mov		r10, rbx
 	mov		rbx, rcx
 	mov		rcx, [rcx + 8]
 	jmp		sort_loop
 
 swap:
 	mov		r8, 1
-	mov		rax, [rcx + 8]
-	mov		[rbx + 8], rax
-	mov     [rcx + 8], rbx
-	cmp		rbx, [rdi]
-	je		update_head
-	mov		[r10 + 8], rcx
-	jmp		swap_registers
-
-
-update_head:
-	mov		[rdi], rcx
-
-swap_registers:
-	; mov rax, rbx
-	; mov rbx, rcx
-	; mov rcx, rax
+	mov		rax, [rcx]
+	mov		r10, [rbx]
+	mov     [rcx], r10
+	mov		[rbx], rax
 	jmp		increment_loop
 
 exit:
 	ret
-
-
-; def bub_sort_linkchange(self):
-;     end = None
-;     while end != self.start_node:
-;         previous = self.start_node
-;         current = self.start_node
-;         while current.next != end:
-;             next = current.next
-;             if current.item > next.item:
-;                 current.next = next.next
-;                 next.next = current
-;                 if current != self.start_node:
-;                     previous.next = next
-;                 else:
-;                     self.start_node = next
-;                 temp = current
-;                 current = next
-;                 next = temp
-;             previous = current
-;             current = current.next
-;         end = current
-; https://stackabuse.com/sorting-and-merging-single-linked-list/
-; [rdi] == HEAD
-; r10 == previous
-; rbx == current
-; rcx == next
-; rsi == cmp
-; r8 == sorted?
-
-
-; while not sorted
-; 	current = HEAD
-; 	next = current
-; 	while next
-; 		next = current.next
-; 		if next.data < current.data
-; 			current.next = next.next
-; 			next.next = current
-; 			sorted = false
