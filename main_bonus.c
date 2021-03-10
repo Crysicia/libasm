3315bac7cff2e66bc5521d25a22084c5bc7b10f9
Crysicia <7459685+Crysicia@users.noreply.github.com>
2021-03-10 15:59:09 +0100

    Tester is ok, need to fix some bugs

--- main_bonus.c
+++ main_bonus.c
@@ -20,16 +20,7 @@ void	ft_list_push_front(t_list **alst, void *data);
 void 	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void*));
 void	ft_list_sort(t_list **begin_list, int (*cmp)());
 
-void display_list(t_list *list)
-{
-	while (list)
-	{
-		printf("----- %p\nContent: [%s]\nContent address: %p\nNext: %p\n", list, (char *)list->data, list->data, list->next);
-		list = list->next;
-	}
-}
-
-t_list **generate_list(void)
+t_list **generate_7_nodes(void)
 {
 	t_list **list = malloc(sizeof(t_list *));
 	ft_list_push_front(list, strdup("aaaa"));
@@ -42,16 +33,24 @@ t_list **generate_list(void)
 	return (list);
 }
 
-void free_list(t_list *list)
+t_list **generate_5_nodes(void)
 {
-	t_list *old;
-	while (list)
-	{
-		free(list->data);
-		old = list;
-		list = list->next;
-		free(old);
-	}
+	t_list **list = malloc(sizeof(t_list *));
+	ft_list_push_front(list, strdup("Epsilon"));
+	ft_list_push_front(list, strdup("Delta"));
+	ft_list_push_front(list, strdup("Gamma"));
+	ft_list_push_front(list, strdup("Beta"));
+	ft_list_push_front(list, strdup("Alpha"));
+	return (list);
+}
+
+t_list **generate_3_nodes(void)
+{
+	t_list **list = malloc(sizeof(t_list *));
+	ft_list_push_front(list, strdup("3rd"));
+	ft_list_push_front(list, strdup("2nd"));
+	ft_list_push_front(list, strdup("1st"));
+	return (list);
 }
 
 void print_list(t_list *list)
@@ -71,7 +70,7 @@ void test_list_size(t_list *list, int expected_size)
 
 	if (output == expected_size)
 		status = GREEN;
-	printf("%s(Expected): %4d, (Output): %4d\n-- List:\n-- ", status, expected_size, output);
+	printf("%s(Expected): %5d, (Output): %5d\n-- List:\n-- ", status, expected_size, output);
 	print_list(list);
 	printf(RESET);
 }
@@ -84,7 +83,7 @@ void test_list_push_front(t_list *list, void *data, int expected_size)
 	int size_after = ft_list_size(list);
 	if (size_after == expected_size)
 		status = GREEN;
-	printf("%s(Expected): %4d, (Output): %4d\n-- List:\n-- ", status, expected_size, size_after);
+	printf("%s(Expected): %5d, (Output): %5d\n-- List:\n-- ", status, expected_size, size_after);
 	print_list(list);
 	printf(RESET);
 }
@@ -97,7 +96,7 @@ void test_list_remove_if(t_list **list, char *to_remove, int expected_size)
 	int size_after = ft_list_size(*list);
 	if (size_after == expected_size)
 		status = GREEN;
-	printf("%s(Expected): %4d, (Output): %4d\n-- List:\n-- ", status, expected_size, size_after);
+	printf("%s(Expected): %5d, (Output): %5d\n-- List:\n-- ", status, expected_size, size_after);
 	print_list(*list);
 	printf(RESET);
 }
@@ -124,90 +123,56 @@ void test_atoi_base(char *str, char *base, int expected)
 	int output = ft_atoi_base(str, base);
 	if (expected == output)
 		status = GREEN;
-	printf("%s(Expected): %4d, (Output): %4d - [%s], [%s]"RESET"\n", status, expected, output, str, base);
+	printf("%s(Expected): %5d, (Output): %5d - [%s], [%s]"RESET"\n", status, expected, output, str, base);
 }
 
 int main(void)
 {
-	t_list	*list = malloc(sizeof(t_list));
-	t_list	*list_next = malloc(sizeof(t_list));
-	t_list	*list_next_next = malloc(sizeof(t_list));
-	t_list	*list_last = malloc(sizeof(t_list));
 
-	list->data = strdup("w");
-	list->next = list_next;
-	list_next->data = strdup("x");
-	list_next->next = list_next_next;
-	list_next_next->data = strdup("y");
-	list_next_next->next = list_last;
-	list_last->data = strdup("z");
-	list_last->next = NULL;
-	test_list_size(list, 4);
+	printf("--- \e[1;36mft_list_size\e[0m ---\n");
+	t_list **size = generate_7_nodes();
+	test_list_size(*size, 7);
+	t_list **size2 = generate_5_nodes();
+	test_list_size(*size2, 5);
+	t_list **size3 = generate_3_nodes();
+	test_list_size(*size3, 3);
 	test_list_size(NULL, 0);
 
-	test_list_push_front(list, strdup("FRONT"), 5);
-	test_list_push_front(list, NULL, 4);
-	t_list **pptr;
-	pptr = generate_list();
-	test_list_remove_if(pptr, "aaaa", 4);
-	test_list_sort(*pptr);
-	printf("--- \e[1;36mft_atoi_base\e[0m ---\n");
+	printf("--- \e[1;36mft_list_push_front\e[0m ---\n");
+	t_list **push = generate_3_nodes();
+	test_list_push_front(*push, strdup("FRONT"), 4);
+	t_list **push2 = generate_3_nodes();
+	test_list_push_front(*push2, NULL, 3);
+	t_list **push3 = malloc(sizeof(t_list *));
+	test_list_push_front(*push3, "FRONT", 1);
+	test_list_push_front(NULL, NULL, 0);
+
+	printf("--- \e[1;36mft_list_remove_if\e[0m ---\n");
+	// t_list **remove = generate_7_nodes();
+	// test_list_remove_if(remove, "aaaa", 4);
+	t_list **remove2 = generate_7_nodes();
+	test_list_remove_if(remove2, "bbbb", 6);
+	// t_list **remove3 = generate_7_nodes();
+	// test_list_remove_if(remove3, NULL, 7);
+	printf("--- \e[1;36mft_list_sort\e[0m ---\n");
+	// test_list_sort(*remove);
+
+
 	test_atoi_base("1337", "0123456789", 1337);
-	// print_list(*pptr);
-	// display_list(*pptr);
-
-
-
-	// printf("LIST ADDRESS: %p\n", list);
-	// printf("PPTR ADDRESS: %p\n", *pptr);
-	// // ft_list_push_front(pptr, strdup("i"));
-	// // ft_list_push_front(pptr, strdup("h"));
-	// // ft_list_push_front(pptr, strdup("g"));
-	// // ft_list_push_front(pptr, strdup("f"));
-	// // ft_list_push_front(pptr, strdup("e"));
-	// ft_list_push_front(pptr, strdup("a"));
-	// // ft_list_push_front(pptr, strdup("d"));
-	// // ft_list_push_front(pptr, strdup("c"));
-	// // ft_list_push_front(pptr, "DELETE");
-	// ft_list_push_front(pptr, strdup("b"));
-	// printf("PPTR ADDRESS: %p\n", *pptr);
-	// printf("\n\n\n\n\n");
-	// display_list(*pptr);
-	// printf("REMOVING:\n\n\n\n\n");
-	// ft_list_remove_if(pptr, "w", strcmp, free);
-	// ft_list_remove_if(pptr, "x", strcmp, free);
-	// ft_list_remove_if(pptr, "y", strcmp, free);
-	// ft_list_remove_if(pptr, "x", strcmp, free);
-	// printf("\n\n\n\n\n --- AFTER REMOVE ---\n\n\n\n\n");
-	// display_list(*pptr);
-	// printf("\n\n\n\n\n");
-	// printf("---- SORT ----\n\n\n\n");
-	// ft_list_sort(pptr, strcmp);
-	// display_list(*pptr);
-	// free_list(*pptr);
-	// printf("LIST ADDRESS: %p\n", list);
-	// printf("PPTR ADDRESS: %p\n", pptr);
-	// free(list.data);
-	// free(list_next.data);
-	// free(list_last.data);
-	// printf("VALID Return: %i\n", ft_atoi_base("  +23", "0123456789"));
-	// printf("VALID Return: %i\n", ft_atoi_base("loool", "0123456789ABCDEF"));
-	// printf("VALID Return: %i\n", ft_atoi_base("    ---++--aasasd", "0123456789ABCDEF"));
-	// printf("VALID Return: %i\n", ft_atoi_base("--aasasd", "0123456789ABCDEF"));
-	// printf("VALID Return: %i\n", ft_atoi_base("loool", "ponybase"));
-	// printf("INVALID Return: %i\n", ft_atoi_base("loool", "dd"));
-	// printf("INVALID Return: %i\n", ft_atoi_base("loool", "\t0123456789ABCDEF"));
-	// printf("INVALID Return: %i\n", ft_atoi_base("loool", "\n0123456789ABCDEF"));
-	// printf("INVALID Return: %i\n", ft_atoi_base("loool", "\r0123456789ABCDEF"));
-	// printf("INVALID Return: %i\n", ft_atoi_base("loool", "\v0123456789ABCDEF"));
-	// printf("INVALID Return: %i\n", ft_atoi_base("loool", "\f0123456789ABCDEF"));
-	// printf("INVALID Return: %i\n", ft_atoi_base("loool", " 234"));
-	// printf("INVALID Return: %i\n", ft_atoi_base("loool", "+2345"));
-	// printf("INVALID Return: %i\n", ft_atoi_base("loool", "wer. 2"));
-
-
-
-
-
+	printf("--- \e[1;36mft_atoi_base\e[0m ---\n");
+	test_atoi_base("     1337", "0123456789", 1337);
+	test_atoi_base("-1337", "0123456789", -1337);
+	test_atoi_base("+1337", "0123456789", 1337);
+	test_atoi_base("13w37", "0123456789", 13);
+	test_atoi_base("B0B", "0123456789ABCDEF", 2827);
+	test_atoi_base("101", "01", 5);
+	test_atoi_base("", "0123456789", 0);
+	test_atoi_base("1337", "", 0);
+	test_atoi_base("", "", 0);
+	test_atoi_base("1337", "00123456789", 0);
+	test_atoi_base("1337", "-0123456789", 0);
+	test_atoi_base("1337", "+0123456789", 0);
+	test_atoi_base("1337", " 0123456789", 0);
+	test_atoi_base("5", "5", 0);
 	return 0;
 }
